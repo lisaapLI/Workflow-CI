5ff5842e65c8194afacd464a159685f2ceb5b9cc
lisaapLI <lililisaprilia604@gmail.com>
2026-06-07 10:36:48 +0700

    fix: nested run mlflow project

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -46,7 +46,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 mlflow.sklearn.autolog()
 
-with mlflow.start_run():
+with mlflow.start_run(nested=False, run_name="RandomForest_Diabetes_CI"):
 
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
